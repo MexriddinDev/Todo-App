d1ab826f9d56ecc4ab14b7a31a32147ed9ac3972
Mexriddin <mekkinuriddinov@gmail.com>
2024-11-25 18:40:43 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
+    <style>
+        .todo-body{
+            max-width: 700px;
+            box-shadow: 0 0 5px #ccc;
+        }
+        .todo-text {
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="row d-flex justify-content-center">
+        <div class="todo-body my-5 p-3">
+            <h1 class="text-center todo-text">Todo App</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi facere fugit hic nesciunt similique. Alias doloremque eaque esse explicabo. Dolorum ducimus harum libero molestiae necessitatibus optio perspiciatis quos repudiandae sunt?</p>
+            <div class="input-group mb-3">
+                inpu
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
