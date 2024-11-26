683b27e929f4df3388861e68f0677a425bd74000
Mexriddin <mekkinuriddinov@gmail.com>
2024-11-26 14:04:08 +0500

    database added

--- add.php
+++ add.php
@@ -1,24 +1,13 @@
 <?php
-require'Database.php';
+require_once "DB.php";
+$title=$_POST["title"];
+$status=$_POST["status"];
+$due_date=$_POST["due_date"];
 
-
-$title = $_POST['title'];
-
-$status = $_POST['status'];
-
-$due_date = $_POST['due_date'];
-
-$query = "INSERT INTO todos (title,status,due_date)  VALUES (:title, :status, :due_date)";
-
-$db = new Database();
-$stmt = $db->conn->prepare($query);
+$sql = "Insert into todos(title, status, due_date) VALUES (:title, :status, :due_date)";
+$stmt = $con->prepare($sql);
 $stmt->bindParam(':title', $title);
 $stmt->bindParam(':status', $status);
 $stmt->bindParam(':due_date', $due_date);
 $stmt->execute();
-
-
-
-
-
-?>
+header("Location:index.php");
